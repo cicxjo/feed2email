971ce3095854d9e0cb712fef4337cbf8c327eae6
Cyril AUGIER <cicxjo@posteo.net>
2024-04-18 16:17:52 +0200

    chore: add licenses and make Reuse happy

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2024 Cyril AUGIER <cicxjo@posteo.net>, et al.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 FROM docker.io/debian:latest
 
 COPY ../composer.json /
--- test/Autoloader/_Files/A.php
+++ test/Autoloader/_Files/A.php
@@ -1,5 +1,9 @@
 <?php
 
+// SPDX-FileCopyrightText: 2024 Cyril AUGIER <cicxjo@posteo.net>, et al.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 namespace Test\Autoloader\_Files;
 
 class A
